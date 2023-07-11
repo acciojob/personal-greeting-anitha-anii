0126d271d4730ba7f05aa1a030fc99f36f755e96
anitha-anii <anithachowdary355@gmail.com>
2023-07-11 10:39:09 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
 
 const App = () => {
-     const [name, setName] = useState("");
-  
-    const handleNameChange = (e) => {
-      setName(e.target.value);
-    };
-  
-    return (
-      <div>
-        <label htmlFor="name">Enter your name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={handleNameChange}
-        />
-        {name && <p>Hello {name} !</p>}
-      </div>
-    );
+  const [name, setName] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
-  
-  
+
+  return (
+    <div>
+      <label htmlFor="name">Enter your name:</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={handleNameChange}
+      />
+      {name && <p>Hello {name}{' '}!</p>}
+    </div>
+  );
+};
 
 export default App;
+
