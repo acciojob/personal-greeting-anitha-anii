a6a9618a461c1f679daef06766ab8e3d3f952125
anitha-anii <anithachowdary355@gmail.com>
2023-07-12 18:24:27 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
         value={name}
         onChange={handleNameChange}
       />
-      {name && <p>Hello {name}{' '}!</p>}
+      {name && <p>Hello {name}!</p>}
     </div>
   );
 };
